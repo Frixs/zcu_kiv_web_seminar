14f3c5f9f95950df59c767cb7f43f879bd867797
Frixs <frixs@users.noreply.github.com>
2017-10-11 21:45:43 +0200

    Get protocol from the server, Added some phpdoc

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * There you can define global functions.
+ * It is very useful f.e. if you need to call some Lang or Confing in your views. You dont need to define 'use' path.
+ */
+
 use Frixs\Language\Lang;
 
 function langGet($path, $parameters = [])
--- config/app.php
+++ config/app.php
@@ -4,5 +4,5 @@ return [
     'name' => 'Action Calendar',
     'locale' => 'en',
     'developer_mode' => true,
-    'root' => 'http://'. $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\'),
+    'root' => ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\'),
 ];
