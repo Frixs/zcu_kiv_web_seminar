9be3a0ecf10c12205f71ec7a183223e0a9b1b025
FrixsLAP <frixs.dotlog@gmail.com>
2017-10-11 12:07:18 +0200

    Kernel, middleware functionality

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,24 +1,44 @@
 <?php
-//TODO create some basic middleware https://github.com/laravel/laravel/blob/master/app/Http/Kernel.php
+
 namespace App\Http;
 
 class Kernel
 {
+    /**
+     * The application's global middleware stack
+     * These middleware are run during every request to your application
+     *
+     * @var array
+     */
     protected static $middleware = [
+        //'SomeMiddleware',
+        //'SomeMiddleware',
+        //'SomeMiddleware'
     ];
 
+    /**
+     * Undocumented variable
+     *
+     * @var array
+     */
     protected static $middlewareGroups = [
         'web' => [
         ]
     ];
 
+    /**
+     * The application's route middleware
+     * These middleware are used individually on current sections
+     *
+     * @var array
+     */
     protected static $routeMiddleware = [
         'home' => 'Authenticate',
-        'home.index' => 'Authenticate'
+        //'home.index' => ''
     ];
 
     /**
-     * Run all middlewares according to input parameters
+     * Run all middleware according to input parameters
      *
      * @param string $controller
      * @param string $method
@@ -27,21 +47,46 @@ class Kernel
      */
     public static function run($controller, $method, $parameters)
     {
-        if (isset(self::$routeMiddleware[$controller])) {
-            $validator = '\\App\Http\Middleware\\'. self::$routeMiddleware[$controller];
-            new $validator;
-            if (!$validator::validate()) {
+        // Validate global middleware
+        for ($i = 0; $i < count(self::$middleware); $i++) {
+            $middlewareTestClass = '\\App\Http\Middleware\\'. self::$middleware[$i];
+            if (!self::checkMiddleware($middlewareTestClass)) {
                 return false;
             }
         }
 
+        // Validate controller section
+        if (isset(self::$routeMiddleware[$controller])) {
+            $middlewareTestClass = '\\App\Http\Middleware\\'. self::$routeMiddleware[$controller];
+            if (!self::checkMiddleware($middlewareTestClass)) {
+                return false;
+            }
+        }
+
+        // Validate controller's method section
         if (isset(self::$routeMiddleware[$controller .'.'. $method])) {
-            $validator = '\\App\Http\Middleware\\'. self::$routeMiddleware[$controller .'.'. $method];
-            if (!$validator::validate()) {
+            $middlewareTestClass = '\\App\Http\Middleware\\'. self::$routeMiddleware[$controller .'.'. $method];
+            if (!self::checkMiddleware($middlewareTestClass)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    /**
+     * Validate middleware
+     *
+     * @param string $middlewareTestClass   middleware class name including namespace
+     * @return bool
+     */
+    private static function checkMiddleware($middlewareTestClass)
+    {
+        new $middlewareTestClass;
+        if ($middlewareTestClass::validate()) {
+            return true;
+        }
+
+        return false;
     }
 }
