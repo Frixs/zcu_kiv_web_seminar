81fc026a1c7cc27cd8ea0bf4e868c9e568c16ed3
FrixsLAP <frixs@users.noreply.github.com>
2017-11-08 13:35:53 +0100

    Fixed responsive layout, Added small footer fo plain pages, Added master background.

--- resources/views/layouts/plain-form.blade.php
+++ resources/views/layouts/plain-form.blade.php
@@ -4,20 +4,16 @@
 </head>
 <body>
     @include('includes.initials')
-    
-    <header class="row">
-        @include('includes.header')
-    </header>
 
     <div class="container">
-        <div class="form-middle-wrapper jumbotron position-middle col-xs-11 col-sm-8 col-lg-5 col-lg-form-middle-max">
-        <a href="home"><img class="logo img-responsive" src="{{ instance('Config')::get('app.root_images_rel') }}logo/app_logo_color.png" alt="logo" draggable="false"></a>
-            @yield('content')
+        <div class="col-xs-11 col-sm-8 col-lg-5 col-lg-form-middle-max form-middle-wrapper">
+            <a href="home"><img class="logo img-responsive" src="{{ instance('Config')::get('app.root_images_rel') }}logo/app_logo_color.png" alt="logo" draggable="false"></a>
+            <div class="jumbotron col-xs-12">
+                @yield('content')
+            </div>
+            <div class="gc-cleaner"></div>
+            @include('includes.footer-plain-form')
         </div>
     </div>
-
-    <footer class="row">
-        @include('includes.footer')
-    </footer>
 </body>
 </html>
