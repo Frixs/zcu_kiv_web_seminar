b0153bf48a65da2355a5481fc833373783a575d7
FrixsLAP <frixs@users.noreply.github.com>
2017-11-08 14:20:32 +0100

    Added footer lang, Added basic css classes.

--- resources/views/includes/footer-plain-form.blade.php
+++ resources/views/includes/footer-plain-form.blade.php
@@ -1,5 +1,5 @@
 <footer class="plain-form">
     <div class="copyright">
-        <em>&copy; Copyright 2017 @if (date('Y') != '2017') - {{ date('Y') }} @endif. All rights reserved!</em>
+        <em>&copy; Copyright 2017 @if (date('Y') != '2017') - {{ date('Y') }} @endif. {{ lang('footer.rights') }}</em>
     </div>
 </footer>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,17 +5,20 @@
     <body>
         @include('includes.initials')
         
-        <div class="col-md-2">
+        <div class="col-md-2 master-wrapper-left">
+            <div class="logo-wrapper">
+                <a href="home"><img class="logo img-responsive" src="{{ instance('Config')::get('app.root_images_rel') }}logo/app_logo_color_shine.png" alt="logo" draggable="false"></a>
+            </div>
             <footer>
             </footer>
         </div>
-        <div class="col-md-10">
-            <div class="col-xs-12">
+        <div class="col-md-10 master-wrapper-right">
+            <div class="col-xs-12 header-wrapper">
                 <header>
-                    @yield('header')
+                    <h1>@yield('header')</h1>
                 </header>
             </div>
-            <div class="col-xs-12">
+            <div class="col-xs-12 content-wrapper">
                 @yield('content')
             </div>
         </div>
